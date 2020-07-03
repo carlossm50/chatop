032e9aec0f1c6310a2084ec8e5b55fe1dccf0cd7
CARLOS APOLINAR SEVERINO MEJIA <D19A0006@educacion.edu.do>
2020-07-03 10:06:08 -0400

    Mejoras en la interfaz

--- public/chat.js
+++ public/chat.js
@@ -7,21 +7,32 @@ let output = document.getElementById('output');
 let actions = document.getElementById('actions');
 
 
-btn.addEventListener('click', function () {
+const emitbtn = () => {
     socket.emit('chat:message',
         {
             username: username.value,
             message: message.value
         }
     );
+    message.value = '';
+}
+
+btn.addEventListener('click', function () {
+    emitbtn();
 })
 
-message.addEventListener('keypress', function () {
+message.addEventListener('keypress', function (e) {
     socket.emit('chat:typing', username.value);
+
+    if (e.code === "Enter") {
+        emitbtn();
+    }
 })
 socket.on('chat:message', function (data) {
     actions.innerHTML = '';
     output.innerHTML += '<p> ' + data.message + '</p>'
+
+    window.scrollTo(0, output.scrollHeight);
 })
 socket.on('chat:typing', (data) => {
     actions.innerHTML = '<p>' + data + ' is Writing' + '</p>'
